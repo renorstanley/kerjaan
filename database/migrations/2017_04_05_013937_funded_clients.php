c15f7034c318e9b0d56d3319ee411804ae1b52bf
Stanley <stanley@point.red>
2017-04-05 22:14:49 +0700

    add get funded migration and user seeder

---
+++ database/migrations/2017_04_05_013937_funded_clients.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FundedClients extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funded_clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('no')->unsigned();
+            $table->string('full_name');
+            $table->string('mobile_number');
+            $table->string('company');
+            $table->string('currency');
+            $table->string('funding_amount_req');
+            $table->binary('purchases_order');
+            $table->string('email');
+            $table->string('country');
+            $table->string('industry');
+            $table->string('year_established');
+            $table->string('est_annual_revenue');
+            $table->string('assets_to_be_purchased');
+            $table->string('financial_report');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('funded_clients');
+    }
+}
